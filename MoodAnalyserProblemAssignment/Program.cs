43c41ea08435c905c240de2613f17ae9981f1ca8
kiran-sasaki <kiransekar1111@gmail.com>
2022-12-06 15:32:43 +0530

    Added Happy Mood Anlysing Test Function For MoodAnalyser Problem

--- MoodAnalyserProblemAssignment/Program.cs
+++ MoodAnalyserProblemAssignment/Program.cs
@@ -4,10 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            MoodAnalyser mood = new MoodAnalyser();
-            Console.WriteLine("Enter The Mood");
-            string message = Console.ReadLine();
-            MoodAnalyser.AnalyseMood(message);
+           
         }
     }
 }
