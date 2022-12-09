da80c686a175535385da7e3d872b1fb61da55509
kiran-sasaki <kiransekar1111@gmail.com>
2022-12-09 15:17:50 +0530

    Added Reflection Function For MoodAnalyser Problem

---
+++ MoodAnalyserProblemAssignment/MoodAnalayserFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserProblemAssignment
+{
+    public class MoodAnalayserFactory
+    {
+        public static object CreateMoodAnalyse(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executting = Assembly.GetExecutingAssembly();
+                    Type moodAnaylseType = executting.GetType(className);
+                    return Activator.CreateInstance(moodAnaylseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_CLASS, "Class Not Found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.NO_SUCH_METHOD, "Class Not Found");
+            }
+        }
+    }
+}
